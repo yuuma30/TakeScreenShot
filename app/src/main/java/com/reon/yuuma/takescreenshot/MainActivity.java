ccec6baefde4dd8e88e21262871bd4017f3f4e41
yuuma30 <reon.maple10@gmail.com>
2018-01-03 00:51:07 +0900

    スクリーンショット撮影準備

--- app/src/main/java/com/reon/yuuma/takescreenshot/MainActivity.java
+++ app/src/main/java/com/reon/yuuma/takescreenshot/MainActivity.java
@@ -2,18 +2,37 @@ package com.reon.yuuma.takescreenshot;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
     public static int OVERLAY_PERMISSION_REQ_CODE = 1000;
+    public static int SS_PERMISSION_REQ_CODE = 999;
+    public static int mWidth;
+    public static int mHeight;
+    private VirtualDisplay mVirtualDisplay;
+    private Image mImage;
+    public static ImageReader mImageReader; // スクリーンショット用
+    private String TAG = "TakeService";
 
+    private MediaProjectionManager mMediaProjectionManager;
+    public static MediaProjection mMediaProjection;
     private Button mButtonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +44,10 @@ public class MainActivity extends Activity {
             // API23以上
             checkPermission();
         }
+        if (Build.VERSION.SDK_INT >= 21){
+            checkCameraPermission();
+        }
+
 
         mButtonStart = (Button)findViewById(R.id.startbutton);
         mButtonStart.setOnClickListener(new View.OnClickListener() {
@@ -41,11 +64,20 @@ public class MainActivity extends Activity {
 
     @TargetApi(Build.VERSION_CODES.M)
     public void checkPermission() {
-        if (!Settings.canDrawOverlays(this)){
+        if (!Settings.canDrawOverlays(this)) {
             Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
                     Uri.parse("package:" + getPackageName()));
             startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
         }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void checkCameraPermission(){
+        // カメラのパーミッション取得
+        mMediaProjectionManager = (MediaProjectionManager)getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        Intent permissionIntent = mMediaProjectionManager.createScreenCaptureIntent();
+        startActivityForResult(permissionIntent, SS_PERMISSION_REQ_CODE);
     }
 
     @TargetApi(Build.VERSION_CODES.M)
@@ -53,6 +85,32 @@ public class MainActivity extends Activity {
         if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
             if (!Settings.canDrawOverlays(this)) {
                 // 権限許可されていない
+            }
+        } else if(requestCode == SS_PERMISSION_REQ_CODE) {
+            if (resultCode != RESULT_OK) {
+                //パーミッション無し
+                return;
+            }
+            mMediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+            try {
+                if (Build.VERSION.SDK_INT >= 21) {
+                    DisplayMetrics mertics = getResources().getDisplayMetrics();
+                    mWidth = mertics.widthPixels;
+                    mHeight = mertics.heightPixels;
+                    int density = mertics.densityDpi;
+
+                    Log.d(TAG, "setup VirtualDisplay");
+                    if(MainActivity.mMediaProjection == null){
+                        Log.d(TAG,"mMediaProjection:null");
+                    }
+                    mImageReader = ImageReader.newInstance(mWidth, mHeight, ImageFormat.RGB_565, 2);
+                    mVirtualDisplay = MainActivity.mMediaProjection.createVirtualDisplay("Capturing Display",
+                            mWidth, mHeight, density,
+                            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                            mImageReader.getSurface(), null, null);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
             }
         }
     }
--- app/src/main/java/com/reon/yuuma/takescreenshot/TakeService.java
+++ app/src/main/java/com/reon/yuuma/takescreenshot/TakeService.java
@@ -3,16 +3,27 @@ package com.reon.yuuma.takescreenshot;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.ImageFormat;
 import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+
+import java.nio.ByteBuffer;
 
 /**
  * Created by y on 2018/01/02.
@@ -24,6 +35,12 @@ public class TakeService extends Service implements View.OnClickListener {
     WindowManager mWindowManager;
     private int mDpScale;
     private View mView;
+    private View mTakeDetailView;
+    private ImageReader mImageReader; // スクリーンショット用
+    private int mWidth;
+    private int mHeight;
+    private VirtualDisplay mVirtualDisplay;
+    private Image mImage;
 
 
     @Override
@@ -38,6 +55,8 @@ public class TakeService extends Service implements View.OnClickListener {
     public int onStartCommand(Intent intent,int flags, int startId){
         // 右上に配置
         setTakeButton(Gravity.TOP | Gravity.RIGHT);
+        mWidth = MainActivity.mWidth;
+        mHeight = MainActivity.mHeight;
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -56,7 +75,11 @@ public class TakeService extends Service implements View.OnClickListener {
                 break;
             case R.id.closeButton:
                 Log.d(TAG, "closeButton()");
-                onClickloseButton();
+                onClickCloseButton();
+                break;
+            case R.id.takeButton:
+                onClickTakeButton();
+                break;
             default:
         }
     }
@@ -70,7 +93,8 @@ public class TakeService extends Service implements View.OnClickListener {
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_SECURE,
                 PixelFormat.TRANSLUCENT
         );
 
@@ -93,28 +117,85 @@ public class TakeService extends Service implements View.OnClickListener {
         Log.d(TAG, "onClickDetailButton()");
         // レイアウトファイルからInflateするViewを作成
         LayoutInflater layoutInflater = LayoutInflater.from(this);
-        mView = layoutInflater.inflate(R.layout.service_takedetail, null);
+        mTakeDetailView = layoutInflater.inflate(R.layout.service_takedetail, null);
 
         mWindowManager = (WindowManager)getApplicationContext().getSystemService((Context.WINDOW_SERVICE));
         WindowManager.LayoutParams params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_SECURE,
                 PixelFormat.TRANSLUCENT
         );
         params.gravity = Gravity.CENTER;
 
         // Viewを画面上に追加
-        mWindowManager.addView(mView, params);
-        Button closeButton = (Button)mView.findViewById(R.id.closeButton);
-        closeButton.setOnClickListener(this);
+        mWindowManager.addView(mTakeDetailView, params);
+        Button button = (Button)mTakeDetailView.findViewById(R.id.closeButton);
+        button.setOnClickListener(this);
+        button = (Button)mTakeDetailView.findViewById(R.id.takeButton);
+        button.setOnClickListener(this);
     }
 
     /**
      * 閉じるボタン押下
      */
-    private void onClickloseButton(){
+    private void onClickCloseButton(){
+        Log.d(TAG,"onClickCloseButton()");
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        mWindowManager.removeView(mTakeDetailView);
 
+    }
+
+    /**
+     * 写真を撮るボタン
+     */
+    private void onClickTakeButton() {
+        Log.d(TAG,"onClickTakeButton()");
+        try {
+            Bitmap screenshot = getScreenShot();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private Bitmap getScreenShot(){
+        if(MainActivity.mImageReader == null){
+            Log.d(TAG,"mImageReader:null");
+        }
+        // ImagerReaderから画面を取り出す
+        Image image = MainActivity.mImageReader.acquireLatestImage();
+
+        if(image == null) {
+            Log.d(TAG, "image:null");
+            image = mImage;
+        }
+        mImage = image;
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer buffer = planes[0].getBuffer();
+
+        int pixelStride = planes[0].getPixelStride();
+        int rowStride = planes[0].getRowStride();
+        int rowPadding = rowStride - pixelStride * mWidth;
+
+        // バッファからBitmapを作成
+        Bitmap bitmap =Bitmap.createBitmap(
+                mWidth + rowPadding / pixelStride, mHeight,
+                Bitmap.Config.RGB_565);
+        bitmap.copyPixelsFromBuffer(buffer);
+        image.close();
+        return bitmap;
+    }
+
+    @Override
+    public void onDestroy(){
+        if(mVirtualDisplay!=null){
+            Log.d(TAG, "release VirtualDisplay");
+            mVirtualDisplay.release();
+        }
+        super.onDestroy();
     }
 }
